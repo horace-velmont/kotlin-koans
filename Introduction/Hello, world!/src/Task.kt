70ec7081cbea7a0e8c61cd33f664eb9c1c8d7b37
Horace Velmont <dragond7@naver.com>
2019-05-11 13:14:26 +0900

    initial commit - introduction

---
+++ Introduction/Hello, world!/src/Task.kt
@@ -0,0 +1,1 @@
+fun start(): String  { return "OK" }
---
+++ Introduction/Nullable types/src/Task.kt
@@ -0,0 +1,15 @@
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) {
+    val personalInfo = client?.personalInfo
+    val email = personalInfo?.email
+
+    if (email != null && message !=null)
+        mailer.sendMessage(email, message)
+}
+
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
